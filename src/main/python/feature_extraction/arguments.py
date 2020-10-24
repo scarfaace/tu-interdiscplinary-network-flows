50d371dfe6564834e37d2d6b6bc54ece303218c0
Allan Kalnay <a.kalnay@sportradar.com>
2020-10-24 20:19:26 +0200

    dded filepath argument but commented out

--- src/main/python/feature_extraction/arguments.py
+++ src/main/python/feature_extraction/arguments.py
@@ -4,6 +4,7 @@ from argparse import ArgumentParser
 class MyArgumentsParser:
     def __init__(self):
         self.argument_adders = [
+            # FilepathArgument(),
             FeaturesArgument()
         ]
 
@@ -13,6 +14,15 @@ class MyArgumentsParser:
             parser = argument_adder.add_argument_to_parser(parser)
         args = parser.parse_args()
         return args
+
+
+# class FilepathArgument:
+#     @staticmethod
+#     def add_argument_to_parser(parser):
+#         parser.add_argument("--filepath", type=str, required=True, nargs='+',
+#                             help="File path of the network streams in csv.")
+#         return parser
+
 
 
 class FeaturesArgument:
