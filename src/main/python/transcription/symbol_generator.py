961556689aa7a32c5267064c8d0d4382971d7260
Allan Kalnay <a.kalnay@sportradar.com>
2020-10-27 22:09:52 +0100

    working with decimals instead of floats

--- src/main/python/transcription/symbol_generator.py
+++ src/main/python/transcription/symbol_generator.py
@@ -1,12 +1,13 @@
+from decimal import Decimal
 from math import floor
 
 
 class CommunicationGapsGenerator:
     @staticmethod
     def generate(entry, stream_last_time):
-        time_diff = entry.timestamp - stream_last_time
+        time_diff = Decimal(str(entry.timestamp)) - Decimal(str(stream_last_time))
         time_diff_ms = time_diff * 1000
-        time_diff_tens_ms = round(time_diff_ms/10)
+        time_diff_tens_ms = floor(time_diff_ms/10)
         gaps = []
         for tens_ms_inc in range(time_diff_tens_ms):
             gaps.append("-")
