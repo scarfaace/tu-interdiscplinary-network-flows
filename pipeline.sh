fb4b34840140f1d2a8fc34dd5313d50a05f822fd
Allan Kalnay <a.kalnay@sportradar.com>
2020-12-08 17:30:14 +0100

    added go flows feature extraction

--- pipeline.sh
+++ pipeline.sh
@@ -2,16 +2,15 @@
 # Add source roots to PYTHONPATH
 export PYTHONPATH=`pwd`/src/main/python/
 
+pcap_file_path="resources/CIC-IDS-2017/Tuesday-WorkingHours.pcap"
+
 
 # 1. Extracting features from pcap in a tabular format (csv)
-echo "Extracting features from pcap in a tabular format (csv)"
-tshark -n -l -r test.pcap -T pdml | python3 src/main/python/feature_extraction/main.py --features timestamp ip.proto ip.src ip.dst tcp.flags tcp.len 1> out/test.csv
+echo "Extracting features from pcap into a tabular format (csv)"
+go-flows run features feature_extraction/pcap2pkts.json export csv out/Tuesday.csv source libpcap "$pcap_file_path"
+
 
 # 2. Extracting flows as conversation transcriptions
 echo "Extracting flows as conversation transcriptions"
-python3 src/main/python/transcription/main.py --filename out/test.csv --labels-filename in/test_labels.csv > out/test_tr.csv
-
-
-#tshark -n -l -r test.pcap -T pdml | \
-#  python3 src/main/python/feature_extraction/main.py --features timestamp ip.proto ip.src ip.dst tcp.flags tcp.len | \
-#  python3 src/main/python/transcription/main.py test.csv > test_tr.txt
+#python3 src/main/python/transcription/main.py --filename out/test.csv --labels-filename in/test_labels.csv > out/test_tr.csv
+python3 src/main/python/transcription/main.py --filename out/test.csv --labels-filename resources/CIC-IDS-2017/labels_CAIA_17.csv > out/test_tr.csv
