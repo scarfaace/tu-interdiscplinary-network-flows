9e4b9ca0f0d35dde3717527e2493ae88b7934666
Allan Kalnay <a.kalnay@sportradar.com>
2021-03-13 18:39:13 +0100

    awk script for cutting transcriptions at Xth character

---
+++ transcription/utils/transcriptions_length_cut.sh
@@ -0,0 +1,14 @@
+transcription_length="${1:-250000}"
+echo "Cutting at ${transcription_length}th character."
+
+base_file_name="Wednesday_transcription"
+input_transcription_file_path="../out/""$base_file_name"".csv"
+output_file_path="../out/""$base_file_name""_cut.csv"
+
+awk -F '\t' -v transcription_length="$transcription_length" '{
+  if(NR==1) {
+    print $0
+  } else {
+    printf("%s\t%s\t%s\t%s\t%s\n", $1, $2, $3, $4, substr($5, 1, transcription_length));
+  }
+}' "$input_transcription_file_path" > "$output_file_path"
