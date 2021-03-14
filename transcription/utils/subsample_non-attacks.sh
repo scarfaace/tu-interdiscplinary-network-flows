351ba4cffca2bcef32d74912df6ba48870475ea8
Allan Kalnay <a.kalnay@sportradar.com>
2021-03-14 12:46:43 +0100

    updating non attacks subsampling

--- transcription/utils/subsample_non-attacks.sh
+++ transcription/utils/subsample_non-attacks.sh
@@ -1,6 +1,7 @@
 subsample_size="${1:-7500}"
-
 base_file_names=( "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" )
+
+echo "Subsampling a data set of size $subsample_size entries"
 
 for base_file_name in "${base_file_names[@]}"
 do
@@ -8,13 +9,14 @@ do
 
   input_transcription_file_path="../out/${base_file_name}_transcription.tsv"
   tmp_output_file_path="../out/${base_file_name}_non-attacks.tsv"
+  non_attacks_subsample_output_path="../out/${base_file_name}_non-attacks_subsample.tsv"
 
   # extract only non-attacks to a tmp file
   awk -F '\t' '{if(NR==1 || $3==0){print $0}}' "$input_transcription_file_path" > "$tmp_output_file_path"
 
   # pip install subsample
   # subsample non-attacks
-  subsample --header-rows=1 --sample-size "$subsample_size" "$tmp_output_file_path" > "${base_file_name}_non-attacks_subsample.tsv"
+  subsample --header-rows=1 --sample-size "$subsample_size" "$tmp_output_file_path" > "$non_attacks_subsample_output_path"
 
   # delete the tmp file
   rm "$tmp_output_file_path"
