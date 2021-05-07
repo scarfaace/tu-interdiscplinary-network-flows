3678dcb23efd2dc8b660e794bc11ed9415acbff5
Allan Kalnay <a.kalnay@sportradar.com>
2021-05-07 17:11:59 +0200

    date print before and after transcriptions script

--- transcription/pipeline.sh
+++ transcription/pipeline.sh
@@ -3,7 +3,7 @@
 export PYTHONPATH=`pwd`/src/main/python/
 
 
-base_file_name="${1:-Tuesday}"
+base_file_name="${1:-Monday}"
 pcap_file_path="resources/CIC-IDS-2017/${base_file_name}-WorkingHours.pcap"
 feature_extraction_output_file_path="out/${base_file_name}_features.csv"
 out_transcription_file_path="out/${base_file_name}_transcription.tsv"
@@ -16,12 +16,16 @@ echo "Processing file ${pcap_file_path}"
 echo "Extracting features from pcap into a tabular format (csv)"
 go-flows run features "$feature_extraction_config_path" export csv "$feature_extraction_output_file_path" source libpcap "$pcap_file_path"
 echo "Feature extraction finished"
+echo
 
 
 # 2. Extracting flows as conversation transcriptions
 echo "Extracting flows as conversation transcriptions"
+echo `date`
 python3 src/main/python/transcription/main.py --filename "$feature_extraction_output_file_path" --labels-filename resources/CIC-IDS-2017/labels_CAIA_17.csv > "$out_transcription_file_path"
 echo "Finished creating text-like transcriptions of network flows."
+echo `date`
+echo
 
 echo "NTFT saved into ${out_transcription_file_path}"
 
