99c71ab85b899d94217b49554089c1cf4df400a9
Allan Kalnay <a.kalnay@sportradar.com>
2021-08-30 17:27:46 +0200

    fix pipeline-general.sh

--- transcription/pipeline-general.sh
+++ transcription/pipeline-general.sh
@@ -22,7 +22,6 @@ feature_extraction_config_path="feature_extraction/pcap2pkts.json"
 
 echo "Processing file ${pcap_file_path}"
 
-# 1. Extracting features from pcap in a tabular format (csv)
 echo "Extracting features from pcap into a tabular format (csv)"
 go-flows run features "$feature_extraction_config_path" export csv "$feature_extraction_output_file_path" source libpcap "$pcap_file_path"
 echo "Feature extraction finished"
@@ -32,7 +31,7 @@ echo
 # 2. Extracting flows as conversation transcriptions
 echo "Extracting flows as conversation transcriptions"
 echo `date`
-python3 src/main/python/transcription/main.py --filename "$feature_extraction_output_file_path" resources/CIC-IDS-2017/labels_CAIA_17.csv > "$out_transcription_file_path"
+python3 src/main/python/transcription/main.py --filename "$feature_extraction_output_file_path" > "$out_transcription_file_path"
 echo "Finished creating text-like transcriptions of network flows."
 echo `date`
 echo
